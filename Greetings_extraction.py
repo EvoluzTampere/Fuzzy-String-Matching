1d5e52a0b34a5374776b59e101e2b39b36e48b77
Fuzzy String Matching <49145951+EvoluzTampere@users.noreply.github.com>
2019-04-09 13:56:51 +0300

    Update Greetings_extraction.py

--- Greetings_extraction.py
+++ Greetings_extraction.py
@@ -1,3 +1,3 @@
 text="Tampere on Suomen kaupunki ja Pirkanmaan maakuntakeskus rakas, joka sijaitsee Näsijärven ja Pyhäjärven rannoilla. Tampere on väkiluvultaan Suomen kolmanneksi suurin kunta ja lähikuntineen Suomen toiseksi suurin kaupunkialue. Tampere on myös asukasmäärältään Pohjoismaiden suurin sisämaakaupunki"
-[ t for t in text.split('. ') if 'rakas' in t]
-text[0:100]
+[ t for t in text.split('. ') if 'rakas' in t] #select the setence with word 'rakas'
+text[0:100] #select the first 100 char.
